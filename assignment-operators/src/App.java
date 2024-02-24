2d51da5058cb8b868addf2fba54440ebf42f1ccd
Perkles <davifernandosousadias18@gmail.com>
2024-02-24 12:45:21 -0300

    fast operators ternario

--- assignment-operators/src/App.java
+++ assignment-operators/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte a = 10;
+        byte b = 10;
+
+        
+        String result = a==b? "true": "false";
+        System.out.println(result); 
     }
 }
