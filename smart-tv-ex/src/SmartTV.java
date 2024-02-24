56cee671b732d97e9dd7c79b35de77e85ad5c6c6
Perkles <davifernandosousadias18@gmail.com>
2024-02-24 14:27:28 -0300

    class exer smartTV

---
+++ smart-tv-ex/src/SmartTV.java
@@ -0,0 +1,12 @@
+public class SmartTV {
+    boolean on = false;
+    int channel =1;
+    int sons = 50;
+
+    public void onTV(){
+        on = true;
+    }
+    public void offTV(){
+        on = false;
+    }
+}
---
+++ smart-tv-ex/src/Users.java
@@ -0,0 +1,17 @@
+public class Users {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTV smartTV = new SmartTV();
+
+        System.out.println("tv ligada? " + smartTV.on);
+        System.out.println("what channel is here? " + smartTV.channel);
+        System.out.println("what sons is here? " + smartTV.sons);
+
+        smartTV.onTV();
+        System.out.println("tv ligada? " + smartTV.on);
+        smartTV.offTV();
+        System.out.println("tv ligada? " + smartTV.on);
+    }
+
+    
+}
