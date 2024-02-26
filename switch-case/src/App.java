8ebf0445d37181fe5b7f3ce2637b6d9c1c78d0eb
Perkles <davifernandosousadias18@gmail.com>
2024-02-26 18:03:20 -0300

    switch case commands

--- switch-case/src/App.java
+++ switch-case/src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String sigla = "M";
+        
+        switch (sigla){
+            case "a":{
+                System.out.println("A");
+                break;
+            }
+            case "M": {
+                System.out.println("fala");
+                break;
+            }
+            default:{
+                System.out.println("nome");
+            }
+        }
     }
 }
