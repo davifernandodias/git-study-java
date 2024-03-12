f858c1459b71b8543e2048e13b92d89be4e7dd04
Perkles <davifernandosousadias18@gmail.com>
2024-03-12 18:26:10 -0300

    ex 6

--- excercios unip/exercios/src/App.java
+++ excercios unip/exercios/src/App.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("informe sexo: M/F");
+        String sexo = sc.nextLine();
+        sexo = sexo.toLowerCase();
+        System.out.println("informe altura");
+        double altura = sc.nextDouble();
+        if (sexo == "m"){
+            double ideal = (72.7*altura)-58;
+            System.out.println(ideal);
+        } else if (sexo == "f"){
+            double ideal = (62.1*altura)-44.7;
+            System.out.println(ideal);
+        }
+
     }
 }
