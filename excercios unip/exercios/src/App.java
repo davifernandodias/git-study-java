ab7b5cd0ed43cd747e5e1633f271680a47e7b12f
Perkles <davifernandosousadias18@gmail.com>
2024-03-12 20:06:50 -0300

    ex08 fibonacci

--- excercios unip/exercios/src/App.java
+++ excercios unip/exercios/src/App.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("informe sexo: M/F");
-        String sexo = sc.nextLine();
-        sexo = sexo.toLowerCase();
-        System.out.println("informe altura");
-        double altura = sc.nextDouble();
-        if (sexo == "m"){
-            double ideal = (72.7*altura)-58;
-            System.out.println(ideal);
-        } else if (sexo == "f"){
-            double ideal = (62.1*altura)-44.7;
-            System.out.println(ideal);
-        }
+    Scanner sc = new Scanner(System.in);
+    System.out.println("informe final");
+    int n = sc.nextInt();
+    int a = 0,b=1;
+    System.out.print(a+" "+ b + " ");
+    for (int i = 2; i < n; i++){
+        int soma = a + b;
+        System.out.print(soma+ " ");
+        a = b;
+        b = soma;
+
+        
+
+    }
 
     }
 }
