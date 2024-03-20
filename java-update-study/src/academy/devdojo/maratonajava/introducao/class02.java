21486c6c4fdd397bae41c5215f8636918683c24f
Perkles <davifernandosousadias18@gmail.com>
2024-03-20 19:11:07 -0300

    type variable

--- java-update-study/src/academy/devdojo/maratonajava/introducao/class02.java
+++ java-update-study/src/academy/devdojo/maratonajava/introducao/class02.java
@@ -3,5 +3,14 @@ package academy.devdojo.maratonajava.introducao;
  * types primitive variable
  */
 public class class02 {
+    // int, double, float, char, byte, short, long, boolean
     int age = 10;
+    long numberLong = 2000000;
+    double salary = 2000;
+    byte ageByte = 20;
+    short ageShot = 10;
+    boolean verdade = true;
+    boolean falso = false;
+    char caractere = 'd';
+    
 }
