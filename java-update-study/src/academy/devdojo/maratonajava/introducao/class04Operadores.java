b4e081954b96fc87f9514d40b6c5a6b983e2fc45
Perkles <davifernandosousadias18@gmail.com>
2024-03-20 20:50:32 -0300

    retorno de valor inteiro vai sair inteiro

--- java-update-study/src/academy/devdojo/maratonajava/introducao/class04Operadores.java
+++ java-update-study/src/academy/devdojo/maratonajava/introducao/class04Operadores.java
@@ -1,6 +1,9 @@
 package academy.devdojo.maratonajava.introducao;
 public class class04Operadores{
     public static void main(String[] args) {
-        
+        int number01 = 10;
+        int number02 = 20;
+        double result = number01 /(double) number02; // int int retorna int
+        System.err.println(result);
     }
 }
