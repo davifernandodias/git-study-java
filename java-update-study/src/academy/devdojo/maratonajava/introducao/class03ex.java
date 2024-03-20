6c5fb472083c50dca4e393c632fc6e8b925e8ba3
Perkles <davifernandosousadias18@gmail.com>
2024-03-20 19:55:39 -0300

    first ex. out + variables

---
+++ java-update-study/src/academy/devdojo/maratonajava/introducao/class03ex.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class class03ex {
+    public static void main(String[] args) {
+        
+        String name = "davi";
+        String address = "São Paulo, Brasil";
+        double salary = 200.34;
+        String data = "10/10/2000";
+        System.out.println("Eu " + name+ ", morando no endereço " +address+ " confirmo que recebi salário de " +salary+ " ,na data "+data);
+    }
+}
