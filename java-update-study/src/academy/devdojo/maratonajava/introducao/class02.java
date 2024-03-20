1af7de652dc3abcdab2f414cea075155ffc1024d
Perkles <davifernandosousadias18@gmail.com>
2024-03-20 19:49:55 -0300

    update

--- java-update-study/src/academy/devdojo/maratonajava/introducao/class02.java
+++ java-update-study/src/academy/devdojo/maratonajava/introducao/class02.java
@@ -3,14 +3,19 @@ package academy.devdojo.maratonajava.introducao;
  * types primitive variable
  */
 public class class02 {
-    // int, double, float, char, byte, short, long, boolean
-    int age = 10;
-    long numberLong = 2000000;
-    double salary = 2000;
-    byte ageByte = 20;
-    short ageShot = 10;
-    boolean verdade = true;
-    boolean falso = false;
-    char caractere = 'd';
+    public static void main(String[] args) {
+        
+        // int, double, float, char, byte, short, long, boolean
+        int age = 10;
+        long numberLong = 2000000;
+        double salary = 2000.2;
+        float numberFloat = 200.5F;
+        byte ageByte = 20;
+        short ageShot = 10;
+        boolean verdade = true;
+        boolean falso = false;
+        char caractere = 'd';
+    }
+    
     
 }
