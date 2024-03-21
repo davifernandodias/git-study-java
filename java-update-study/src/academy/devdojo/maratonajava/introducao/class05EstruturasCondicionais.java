643d96cd184e0c5b077be738fd846e7420abc59b
Perkles <davifernandosousadias18@gmail.com>
2024-03-21 17:39:23 -0300

    negação !condição ao inves de condição == false

--- java-update-study/src/academy/devdojo/maratonajava/introducao/class05EstruturasCondicionais.java
+++ java-update-study/src/academy/devdojo/maratonajava/introducao/class05EstruturasCondicionais.java
@@ -3,10 +3,14 @@ package academy.devdojo.maratonajava.introducao;
 public class class05EstruturasCondicionais {
     public static void main(String[] args) {
         int age = 17;
+        boolean togoto = 15 > 20;
         if (age > 17){
             System.out.println("vamo pro puteiro");
         }else if(age<=17){
             System.out.println("não vamos pro puteiro");
+        }
+        if (!togoto){ // se a condição for falsa e eu quero afirmar ! negação
+            System.out.println("não e maior");
         }
 
     }
