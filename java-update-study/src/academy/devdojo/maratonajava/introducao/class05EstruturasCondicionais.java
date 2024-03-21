2cd31abc705b40f19a75683613e3f169c5b63c00
Perkles <davifernandosousadias18@gmail.com>
2024-03-21 17:24:24 -0300

    if estrutura

--- java-update-study/src/academy/devdojo/maratonajava/introducao/class05EstruturasCondicionais.java
+++ java-update-study/src/academy/devdojo/maratonajava/introducao/class05EstruturasCondicionais.java
@@ -2,8 +2,12 @@ package academy.devdojo.maratonajava.introducao;
 
 public class class05EstruturasCondicionais {
     public static void main(String[] args) {
-        if (10 > 9){
-            
+        int age = 17;
+        if (age > 17){
+            System.out.println("vamo pro puteiro");
+        }else if(age<=17){
+            System.out.println("não vamos pro puteiro");
         }
+
     }
 }
