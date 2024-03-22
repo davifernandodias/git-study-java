0a7dcdd838c1f9045f935589144c437dd79d88ee
Perkles <davifernandosousadias18@gmail.com>
2024-03-22 19:41:11 -0300

    array cont no finalizado

--- java-update-study/src/academy/devdojo/maratonajava/introducao/class09Matrizes.java
+++ java-update-study/src/academy/devdojo/maratonajava/introducao/class09Matrizes.java
@@ -2,6 +2,9 @@ package academy.devdojo.maratonajava.introducao;
 
 public class class09Matrizes {
     public static void main(String[] args) {
-        
+        int[][] data = new int [13][30];
+        for (int i=0;i<data.length;i++){
+            
+        }
+        }
     }
-}
