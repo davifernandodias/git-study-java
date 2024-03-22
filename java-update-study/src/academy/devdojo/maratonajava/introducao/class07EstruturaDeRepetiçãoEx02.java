dfe0be07540698cdf113dcbedaf2822618680ba0
Perkles <davifernandosousadias18@gmail.com>
2024-03-22 17:09:08 -0300

    ex portion best update

--- java-update-study/src/academy/devdojo/maratonajava/introducao/class07EstruturaDeRepetiçãoEx02.java
+++ java-update-study/src/academy/devdojo/maratonajava/introducao/class07EstruturaDeRepetiçãoEx02.java
@@ -1,15 +1,19 @@
 package academy.devdojo.maratonajava.introducao;
 
-public class class07EstruturaDeRepetiçãoEx02{
-    // value of car get amount x portion
-    // valuePortion >= 100
+public class class07EstruturaDeRepetiçãoEx02 {
+    /**
+     *
+     * value of car get amount x portion
+     * valuePortion >= 1000
+     */
     public static void main(String[] args) {
         int valueTotal = 30000;
-        for (int portion =1; portion<=valueTotal;portion++){
-            double valuePortion = valueTotal/portion;
-            if (valuePortion >=1000){
-                System.out.println("Portion: "+portion+" Value Portion: "+valuePortion);
+        for (int portion = 1; portion <= valueTotal; portion++) {
+            double valuePortion = valueTotal / portion;
+            if (valuePortion < 1000) {
+                break;
             }
+            System.out.println("Portion: " + portion + " Value Portion: " + valuePortion);
         }
     }
 }
