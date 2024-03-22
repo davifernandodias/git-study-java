20b6468399332cc447b707a3f5748b5ede23666f
Perkles <davifernandosousadias18@gmail.com>
2024-03-22 08:29:18 -0300

    ex first

--- java-update-study/src/academy/devdojo/maratonajava/introducao/class05EstruturasCondicionaisEx.java
+++ java-update-study/src/academy/devdojo/maratonajava/introducao/class05EstruturasCondicionaisEx.java
@@ -1,7 +1,17 @@
 package academy.devdojo.maratonajava.introducao;
-
+import java.util.Scanner;
 public class class05EstruturasCondicionaisEx {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Type From amount: ");
+        double amout = sc.nextDouble();
+        if (amout <34712 || amout == 0){
+            amout *= 0.097;
+        } else if (amout >=34712 && amout <= 68507){
+            amout *= 0.3735;
+        } else {
+            amout*=0.495;
+        }
+        System.out.println(amout);
     }
 }
