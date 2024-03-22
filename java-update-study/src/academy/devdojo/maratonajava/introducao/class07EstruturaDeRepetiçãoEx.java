6d4944ad9214615ded5381b8787f1dc1d71f9a23
Perkles <davifernandosousadias18@gmail.com>
2024-03-22 16:36:45 -0300

    ex estrutura de rep

--- java-update-study/src/academy/devdojo/maratonajava/introducao/class07EstruturaDeRepetiçãoEx.java
+++ java-update-study/src/academy/devdojo/maratonajava/introducao/class07EstruturaDeRepetiçãoEx.java
@@ -2,8 +2,15 @@ package academy.devdojo.maratonajava.introducao;
 
 public class class07EstruturaDeRepetiçãoEx {
     public static void main(String[] args) {
-        for (int i = 0; i<100000000; i+=2){
+        for (int i = 0; i<10; i+=2){
             System.out.println(i);
+        }
+        System.out.println("=======================");
+        for (int j =1; j<50;j++){
+            if (j>25){
+                break;
+            }
+            System.out.println(j);
         }
     }
 }
