5ef824140544440fca0e4c0a8bc5ad4cf90859b5
Perkles <davifernandosousadias18@gmail.com>
2024-03-22 18:31:36 -0300

    Array add for type default primitivos

--- java-update-study/src/academy/devdojo/maratonajava/introducao/class08Arrays01.java
+++ java-update-study/src/academy/devdojo/maratonajava/introducao/class08Arrays01.java
@@ -3,6 +3,11 @@ package academy.devdojo.maratonajava.introducao;
 public class class08Arrays01 {
     public static void main(String[] args) {
         int [] ages= new int[3];
-        System.out.println(ages);
+        ages[0] = 10;
+        ages[1]= 20;
+        ages[2]= 30;
+        System.out.println(ages[0]);
+        System.out.println(ages[1]);
+        System.out.println(ages[2]);
     }
 }
---
+++ java-update-study/src/academy/devdojo/maratonajava/introducao/class08Arrays02.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class class08Arrays02 {
+    /**
+     * Values Default
+     * - byte, short, int, long, float and double 0
+     * - char '\u0000' line white
+     * - boolean false
+     * - String null
+     */
+    public static void main(String[] args) {
+        int[] ages = new int[3];
+        System.out.println(ages[0]);
+        System.out.println(ages[1]);
+        System.out.println(ages[2]);
+    }
+}
