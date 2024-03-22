e1ff91fe40159c5b45e399f6ae9742c95e131c58
Perkles <davifernandosousadias18@gmail.com>
2024-03-22 08:40:39 -0300

    switch case java

---
+++ java-update-study/src/academy/devdojo/maratonajava/introducao/class06SwitchCase.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.introducao;
+import java.util.Scanner;
+public class class06SwitchCase {
+    public static void main(String[] args) {
+        // imprimir dia da semana
+        Scanner sc = new Scanner(System.in);
+        byte day = sc.nextByte();
+        // char, int, byte, short, enum, String
+        switch(day){
+            case 1:System.out.println("domingo"); break;
+            case 2:System.out.println("segunda"); break;
+            case 3:System.out.println("terça"); break;
+            case 4:System.out.println("quarta"); break;
+            case 5:System.out.println("quinta"); break;
+            case 6:System.out.println("sexta"); break;
+            case 7:System.out.println("sábado"); break;
+        }
+    }
+}
