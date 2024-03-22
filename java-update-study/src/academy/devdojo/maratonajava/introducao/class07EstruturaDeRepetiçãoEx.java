a2c2e382ec6427083c6c851e6872a1aed52b1564
Perkles <davifernandosousadias18@gmail.com>
2024-03-22 09:22:11 -0300

    ex rep 0%2==0

---
+++ java-update-study/src/academy/devdojo/maratonajava/introducao/class07EstruturaDeRepetiçãoEx.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class class07EstruturaDeRepetiçãoEx {
+    public static void main(String[] args) {
+        for (int i = 0; i<100000000; i+=2){
+            System.out.println(i);
+        }
+    }
+}
